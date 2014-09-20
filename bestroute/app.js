cec92043289e4353ac4072d3591a23969405f2fa
MALLIKA SEN <mallikasen@lawn-128-61-122-3.lawn.gatech.edu>
2014-09-20 15:26:30 -0400

    multiple textboxes - first row

--- bestroute/app.js
+++ bestroute/app.js
@@ -7,22 +7,11 @@ function InlineEditorController($scope){
 	// its properties available to the view as
 	// variables. Here we set some default values:
 
-	$scope.showtooltip = false;
-	$scope.value = 'Edit me.';
+	$scope.showtooltip = true;
+    $scope.showlocation = true;
+    $scope.showlocation = true;
+    $scope.value = 'to do';
+    $scope.value_loc = 'where';
+    $scope.value_time = 'Whatdeadline';
 
-	// Some helper functions that will be
-	// available in the angular declarations
-
-	$scope.hideTooltip = function(){
-
-		// When a model is changed, the view will be automatically
-		// updated by by AngularJS. In this case it will hide the tooltip.
-
-		$scope.showtooltip = false;
-	}
-
-	$scope.toggleTooltip = function(e){
-		e.stopPropagation();
-		$scope.showtooltip = !$scope.showtooltip;
-	}
 }
