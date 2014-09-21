27eb201b9906bc9434693c8e019a051f8aa243a4
Michelle <mrojass1028@gmail.com>
2014-09-20 21:21:59 -0400

    Tasks updated

--- main.js
+++ main.js
@@ -2,21 +2,12 @@ var QuickRun = angular.module("QuickRun", ["firebase"]);
 
 var navList = angular.module('navList', []);
 
-navList.comtroller('navCtrl', ['$scope', '$location', function($scope, $location) {
-	$scope.navClass = function (page) {
-		var currentRoute = $location.path().substring(1) || 'home';
-		return page === currentRoute ? 'active' : '';
-	}
-}]);
+var TaskController = function($scope) {
+	$scope.tasks = [];
+
+	$scope.add = function() {
+		$scope.tasks.push(task);
+	};
 
 
-var layer = L.mapbox.tileLayer('examples.map-i86nkdio');
-layer.on('ready', function() {
-  // the layer has been fully loaded now, and you can
-  // call .getTileJSON and investigate its properties
-});
-
-var layer = L.mapbox.tileLayer('examples.map-i86nkdio');
-layer.on('error', function(err) {
-  // Handle error
-});
+}
