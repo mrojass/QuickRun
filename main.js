5bc006e4841c4ab0c995005af80cdc98a10b628c
MALLIKA SEN <mallikasen@lawn-128-61-122-3.lawn.gatech.edu>
2014-09-21 02:55:37 -0400

    broken code - add rows`

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ var QuickRun = angular.module("QuickRun", ["firebase"]);
 var navList = angular.module('navList', []);
 
 var TaskController = function($scope) {
-	$scope.tasks = [];
+	$scope.tasks = [{job:"test", location:"Howey", deadline:"11:00am"}];
+	
+$scope.addTask = function(){
+    var task = {
+        job: $scope.job,
+        location: $scope.location,
+        deadline: $scope.deadline,
+    };
+    $scope.tasks.push(task);
+  }; 
 
-	$scope.add = function() {
-		$scope.tasks.push(task);
-	};
+  $scope.removeTask = function(index){
+    $scope.tasks.splice(index, 1);
+  }; 
 
 }
